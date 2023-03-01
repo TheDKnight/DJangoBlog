779aa7df712cb68a814f2c8380acd33f9c21c3fb
TheDKnight <113733655@umail.ucc.ie>
2023-03-01 16:48:40 +0000

    updated views window

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
 STATUS = ((0, "Draft"), (1, "Published"))
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
